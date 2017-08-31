02a911324100c29842e802c68e8d63011c1a25e5
hyunju <hjko_o@daycore.co.kr>
2017-08-31 18:19:35 +0900

    Header, Content 컴포넌트 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from "./Header";
+import Content from "./Content";
 
 class App extends React.Component {
-    render(){
+    render() {
         return (
-            <h1>Hello React Skeleton</h1>
+            <div>
+                <Header/>
+                <Content/>
+            </div>
         );
     }
 }
